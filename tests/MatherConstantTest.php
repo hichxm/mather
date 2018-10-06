42830a2033e00262fdaa269ad67252ff6ac3892b
Hichxm <hicham.slimani.fr@gmail.com>
2018-10-06 13:35:28 +0200

    test: test the new constant

--- tests/MatherConstantTest.php
+++ tests/MatherConstantTest.php
@@ -30,4 +30,10 @@ class MatherConstantTest extends \PHPUnit\Framework\TestCase
         $this->assertSame(0.261497212847642783755426838608695859, Mather::MEISSEL_MERTENS());
     }
 
+    /** @test */
+    public function check_if_stefanboltzmann_returned()
+    {
+        $this->assertSame(5.670367 * pow(10, -8), Mather::STEFAN_BOLTZMANN());
+    }
+
 }
