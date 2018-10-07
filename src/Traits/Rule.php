778ef5fb64f69ad4184e7f74124a4868095c25af
Hichxm <hicham.slimani.fr@gmail.com>
2018-10-07 14:54:03 +0200

    feat: add stefan rules to shortcuts rule

--- src/Traits/Rule.php
+++ src/Traits/Rule.php
@@ -5,6 +5,7 @@ namespace Hichxm\Mather\Traits;
 
 
 use Hichxm\Mather\Classes\Rules\PythagorasRule;
+use Hichxm\Mather\Classes\Rules\StefanRule;
 
 /**
  * Trait Rule
@@ -14,6 +15,7 @@ trait Rule
 {
 
     /**
+     * @see PythagorasRule::class
      * @param float|null $AdjacentOrNull
      * @param float|null $HypotenuseOrNull
      * @param float|null $OppositeOrNull
@@ -22,6 +24,19 @@ trait Rule
     static public function PythagorasRule(?float $AdjacentOrNull, ?float $HypotenuseOrNull, ?float $OppositeOrNull): PythagorasRule
     {
         return new PythagorasRule($AdjacentOrNull, $HypotenuseOrNull, $OppositeOrNull);
+    }
+
+    /**
+     * @see StefanRule::class
+     * @param float|null $ConstantOfStefanOrNull
+     * @param float|null $AreaOrNull
+     * @param float|null $TemperatureOrNull
+     * @param float|null $PowerOrNull
+     * @return StefanRule
+     */
+    static public function StefanRule(?float $ConstantOfStefanOrNull, ?float $AreaOrNull, ?float $TemperatureOrNull, ?float $PowerOrNull): StefanRule
+    {
+        return new StefanRule($ConstantOfStefanOrNull, $AreaOrNull, $TemperatureOrNull, $PowerOrNull);
     }
 
 }
