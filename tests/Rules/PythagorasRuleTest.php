d18b1a7d4fcfa868a2e224d8b0e60d9730279c01
Hichxm <hicham.slimani.fr@gmail.com>
2018-10-07 10:54:32 +0200

    test: check all function of new rule class

---
+++ tests/Rules/PythagorasRuleTest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+use Hichxm\Mather\Classes\Rules\PythagorasRule;
+use PHPUnit\Framework\TestCase;
+
+class PythagorasRuleTest extends TestCase
+{
+
+    /** @test */
+    public function get_hypotenuse_from_adjacent_and_opposite()
+    {
+
+        $arrayOfNumbers = [
+            [10, null, 25, 26.93],
+            [5.5, null, 8.8, 10.38]
+        ];
+
+        foreach ($arrayOfNumbers as $arrayOfNumber) {
+            $rule = new PythagorasRule($arrayOfNumber[0], $arrayOfNumber[1], $arrayOfNumber[2]);
+            $this->assertSame(round($rule->getHypotenuseFromAdjacentAndOpposite(), 2), $arrayOfNumber[3]);
+        }
+
+    }
+
+    /** @test */
+    public function get_adjacent_from_hypotenuse_and_opposite()
+    {
+
+        $arrayOfNumbers = [
+            [null, 45, 25, 37.42],
+            [null, 15, 8.8, 12.15]
+        ];
+
+        foreach ($arrayOfNumbers as $arrayOfNumber) {
+            $rule = new PythagorasRule($arrayOfNumber[0], $arrayOfNumber[1], $arrayOfNumber[2]);
+            $this->assertSame(round($rule->getAdjacentFromHypotenuseAndOpposite(), 2), $arrayOfNumber[3]);
+        }
+
+    }
+
+    /** @test */
+    public function get_opposite_from_hypotenuse_and_adjacent()
+    {
+
+        $arrayOfNumbers = [
+            [12.2, 45, null, 43.31],
+            [98.5, 200, null, 174.06]
+        ];
+
+        foreach ($arrayOfNumbers as $arrayOfNumber) {
+            $rule = new PythagorasRule($arrayOfNumber[0], $arrayOfNumber[1], $arrayOfNumber[2]);
+            $this->assertSame(round($rule->getOppositeFromHypotenuseAndAdjacent(), 2), $arrayOfNumber[3]);
+        }
+
+    }
+}
